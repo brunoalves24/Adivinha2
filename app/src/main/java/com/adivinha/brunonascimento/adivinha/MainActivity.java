4e7a3b36182fbe1b58879b2c825db4c760eae4ca
Bruno Nascimento <bruno.alves24@fatecitu.edu.br>
2018-03-10 15:10:30 -0300

    Jogo

---
+++ app/src/main/java/com/adivinha/brunonascimento/adivinha/MainActivity.java
@@ -0,0 +1,37 @@
+package com.adivinha.brunonascimento.adivinha;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button botaoJogar;
+    private TextView textoResultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        botaoJogar = findViewById(R.id.btn_jogar);
+        textoResultado = findViewById(R.id.textView3);
+
+        //textoResultado.setText("Texto alterado");
+
+        botaoJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random randomico = new Random();
+                int numeroAleatorio = randomico.nextInt(10);
+
+                textoResultado.setText("Número escolhido: " + numeroAleatorio);
+            }
+        });
+
+    }
+}
